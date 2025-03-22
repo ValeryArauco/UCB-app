76fe34167fc35185a4a3420bbe5ace668cca9f59
Jhoss-dev <jhoselin@lunapets.co>
2025-03-22 12:36:44 -0400

    Agregando menu nav desplegable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.transition)
     implementation(libs.room.ktx)
+    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
