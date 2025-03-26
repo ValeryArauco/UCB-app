ff6a348d4b13d8e52255d789c635db783563c2de
Jhoss-dev <jhoselin@lunapets.co>
2025-03-26 15:40:05 -0400

    Restaurando cambios de menú y vistas después del rebase fallido

--- data/src/main/java/com/example/data/service/AuthService.kt
+++ data/src/main/java/com/example/data/service/AuthService.kt
@@ -1,21 +1,21 @@
-package com.uob.framework.service
-
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
-interface AuthService {
-    suspend fun logout()
-}
-
-class AuthServiceImpl : AuthService {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("https://yourapi.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    private val api = retrofit.create(AuthApi::class.java)
-
-    override suspend fun logout() {
-        api.logout()
-    }
-}
+//package com.uob.framework.service
+//
+//import retrofit2.Retrofit
+//import retrofit2.converter.gson.GsonConverterFactory
+//
+//interface AuthService {
+//    suspend fun logout()
+//}
+//
+//class AuthServiceImpl : AuthService {
+//    private val retrofit = Retrofit.Builder()
+//        .baseUrl("https://yourapi.com/")
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .build()
+//
+//    private val api = retrofit.create(AuthApi::class.java)
+//
+//    override suspend fun logout() {
+//        api.logout()
+//    }
+//}
